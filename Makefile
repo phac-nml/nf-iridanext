60df0450a078940ce73b9872269c891a87358648
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2026-02-19 13:57:40 -0600

    Restructuring code to be consistent with new nextflow repository structure

--- Makefile
+++ Makefile
@@ -1,65 +1,21 @@
-
-config ?= compileClasspath
-
-ifdef module 
-mm = :${module}:
-else 
-mm = 
-endif 
+# Build the plugin
+assemble:
+	./gradlew assemble
 
 clean:
+	rm -rf .nextflow*
+	rm -rf work
+	rm -rf build
 	./gradlew clean
 
-compile:
-	./gradlew :nextflow:exportClasspath compileGroovy
-	@echo "DONE `date`"
-
-
-check:
-	./gradlew check
-
-
-#
-# Show dependencies try `make deps config=runtime`, `make deps config=google`
-#
-deps:
-	./gradlew -q ${mm}dependencies --configuration ${config}
-
-deps-all:
-	./gradlew -q dependencyInsight --configuration ${config} --dependency ${module}
-
-#
-# Refresh SNAPSHOTs dependencies
-#
-refresh:
-	./gradlew --refresh-dependencies 
-
-#
-# Run all tests or selected ones
-#
+# Run plugin unit tests
 test:
-ifndef class
-	./gradlew ${mm}test
-else
-	./gradlew ${mm}test --tests ${class}
-endif
+	./gradlew test
 
-#
-# generate build zips under build/plugins
-# you can install the plugin copying manually these files to $HOME/.nextflow/plugins
-#
-buildPlugins:
-	./gradlew copyPluginZip
+# Install the plugin into local nextflow plugins dir
+install:
+	./gradlew install
 
-#
-# Upload JAR artifacts to Maven Central
-#
-upload:
-	./gradlew upload
-
-
-upload-plugins:
-	./gradlew plugins:upload
-
-publish-index:
-	./gradlew plugins:publishIndex
+# Publish the plugin
+release:
+	./gradlew releasePlugin
---
+++ src/test/groovy/phacnml/plugin/iridanext/MetadataParserCSVTest.groovy
@@ -0,0 +1,106 @@
+package phacnml.plugin.iridanext
+
+import java.nio.file.FileSystems
+
+import phacnml.plugin.iridanext.MetadataParser
+import phacnml.plugin.iridanext.MetadataParserCSV
+import spock.lang.Specification
+import groovy.lang.MissingPropertyException
+
+import phacnml.plugin.iridanext.TestHelper
+
+class MetadataParserCSVTest extends Specification {
+
+    def 'Test parse CSV file' () {
+        when:
+        def csvContent = """a,b,c
+                           |1,2,3
+                           |4,5,6""".stripMargin()
+        def csvFile = TestHelper.createInMemTempFile("temp.csv", csvContent)
+        def parser = new MetadataParserCSV("a", ",")
+        def csvMapColA = parser.parseMetadata(csvFile)
+
+        parser = new MetadataParserCSV("b", ",")
+        def csvMapColB = parser.parseMetadata(csvFile)
+
+        then:
+        csvMapColA == [
+            "1": ["b": "2", "c": "3"],
+            "4": ["b": "5", "c": "6"]
+        ]
+        csvMapColB == [
+            "2": ["a": "1", "c": "3"],
+            "5": ["a": "4", "c": "6"]
+        ]
+    }
+
+    def 'Test parse CSV file alternative separator' () {
+        when:
+        def csvContent = """a;b;c
+                           |1;2;3
+                           |4;5;6""".stripMargin()
+        def csvFile = TestHelper.createInMemTempFile("temp.csv", csvContent)
+        def parser = new MetadataParserCSV("a", ";")
+        def csvMap = parser.parseMetadata(csvFile)
+
+        then:
+        csvMap == [
+            "1": ["b": "2", "c": "3"],
+            "4": ["b": "5", "c": "6"]
+        ]
+    }
+
+    def 'Test matchAndParse CSV file' () {
+        when:
+        def csvContent = """a,b,c
+                           |1,2,3
+                           |4,5,6""".stripMargin()
+        def csvFile = TestHelper.createInMemTempFile("tempMatchAndParse.csv", csvContent)
+        def pathMatcherMatch = FileSystems.getDefault().getPathMatcher("glob:**/tempMatchAndParse.csv")
+        def parserMatch = new MetadataParserCSV("a", ",", pathMatcherMatch)
+        def csvMapMatch = parserMatch.matchAndParseMetadata([csvFile])
+
+        def pathMatcherUnmatch = FileSystems.getDefault().getPathMatcher("glob:**/tempMatchAndParse-unmatch.csv")
+        def parserUnmatch = new MetadataParserCSV("a", ",", pathMatcherUnmatch)
+        def csvMapUnmatch = parserUnmatch.matchAndParseMetadata([csvFile])
+
+        then:
+        csvMapMatch == [
+            "1": ["b": "2", "c": "3"],
+            "4": ["b": "5", "c": "6"]
+        ]
+        csvMapUnmatch == [:]
+    }
+
+    def 'Test parse CSV file with missing values' () {
+        when:
+        def csvContent = """a,b,c
+                           |1,2,
+                           |4,,""".stripMargin()
+        def csvFile = TestHelper.createInMemTempFile("temp.csv", csvContent)
+        def parser = new MetadataParserCSV("a", ",")
+        def csvMapColA = parser.parseMetadata(csvFile)
+
+        then:
+        csvMapColA == [
+            "1": ["b": "2", "c": ""],
+            "4": ["b": "", "c": ""]
+        ]
+    }
+
+    def 'Test parse CSV file with missing ids' () {
+        when:
+        def csvContent = """a,b,c
+                           |1,2,3
+                           |4,,6""".stripMargin()
+        def csvFile = TestHelper.createInMemTempFile("temp.csv", csvContent)
+
+        parser = new MetadataParserCSV("b", ",")
+        def csvMapColB = parser.parseMetadata(csvFile)
+
+        then:
+        // the column of identifiers is column "b", which has a missing value
+        // and so should trigger an exception
+        thrown(MissingPropertyException)
+    }
+}
